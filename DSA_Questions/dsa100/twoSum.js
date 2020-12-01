d222c35699d45df3daf7159162fbfdf318ebff10
github username <github email address>
2020-12-01 16:41:20 -0500

    ruby solution

--- DSA_Questions/dsa100/twoSum.js
+++ DSA_Questions/dsa100/twoSum.js
@@ -12,3 +12,8 @@ const twoSum = (nums, target) => {
 		indexOf[nums[i]] = i;
 	}
 };
+
+let nums = [2, 7, 11, 15];
+let target = 9;
+
+console.log(twoSum(nums, target));
--- DSA_Questions/dsa100/twoSum.rb
+++ DSA_Questions/dsa100/twoSum.rb
@@ -1,14 +1,17 @@
 nums = [2,7,11,15]
-target = 9
+target = 26
 
 
 def two_sum(nums, target)
-
-  nums.each_with_index do |nums, index|
-    
+  indexOf = {}
+  nums.each_with_index do |n, i|
+    if indexOf[target - n]
+      return indexOf[target - n], i
+    end
+    indexOf[n] = i
   end
 end
-puts two_sum(nums, target)
+p two_sum(nums, target)
 
 # colors = ['red', 'green', 'blue']
 # colors.each_with_index do |item, index|
