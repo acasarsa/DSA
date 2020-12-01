aeb688509e815548f180eec13e77793b256cd050
github username <github email address>
2020-12-01 16:31:37 -0500

    twoSum

---
+++ DSA_Questions/dsa100/twoSum.rb
@@ -0,0 +1,16 @@
+nums = [2,7,11,15]
+target = 9
+
+
+def two_sum(nums, target)
+
+  nums.each_with_index do |nums, index|
+    
+  end
+end
+puts two_sum(nums, target)
+
+# colors = ['red', 'green', 'blue']
+# colors.each_with_index do |item, index|
+# 	p "#{index}:#{item}" 
+# end
